20ff3e6e939f9404c0255fe062a2cf8ceb56ef51
Maylen Nunez <maylennunez@hotmail.com>
2020-03-28 15:23:31 -0400

    table added

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const indexRoutes = require('./routes/index');
 // connecting to db
 
 mongoose.connect('mongodb://localhost/crud-mongo')
-.then(db => console.log('Db connected'))
+.then(db => console.log('db connected'))
 .catch(err => console.log(err))
 // Settings
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,10 @@ router.get('/', (req, res) => {
 res.render('index');
 });
 
-
+router.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send('received');
+})
 
 
 
