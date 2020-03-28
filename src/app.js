96ed18c9cb5ac98a68a7f3aa7e2c7c12b9cd7215
Maylen Nunez <maylennunez@hotmail.com>
2020-03-27 21:35:24 -0400

    task added

--- src/app.js
+++ src/app.js
@@ -2,22 +2,30 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 
 // Importing routes
+const indexRoutes = require('./routes/index');
 
+// connecting to db
+
+mongoose.connect('mongodb://localhost/crud-mongo')
+.then(db => console.log('Db connected'))
+.catch(err => console.log(err))
 // Settings
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
 
-//middlewares (execute a function before go to the toutes)
+// middlewares (execute a function before go to the toutes)
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
 
+// routes
 
-//routes
-
-
+app.use('/', indexRoutes);
 
 //starting the server
 app.listen(app.get('port'), () => {
