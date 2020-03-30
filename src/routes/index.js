3cd75dec99471e6f2aeea72b547c6408cb807b59
Maylen Nunez <maylennunez@hotmail.com>
2020-03-29 22:13:57 -0400

    edit btn

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,15 +20,28 @@ router.post('/add', async(req, res) => {
     res.send(redirect('/'));
 })
 
+// done btn 
+
 router.get('/done/:id', async (req, res ) =>{
     const {id} = req.params;
-    const task = await Task.findById();   // to find id
+    const task = await Task.findById();       // to find id
     // console.log(task);
-    task.status = !task.status;           // change status  
-     await task.save();                  // save status
+    task.status = !task.status;               // change status  
+     await task.save();                      // save status
      res.send(redirect('/'));
 })
 
+// edit btn
+
+router.get('/edit/:id', async (req, res) => {
+    const {id} = req.params;
+    const task = await Task.findById(); 
+    res.render('edit', {
+        task
+    });
+});
+
+// delete btn
 
 router.get('/delete/:id', async( req, res) => {
 const {id} = req.params;
